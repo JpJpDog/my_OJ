07532e51868982d33ab325fe05f1eb0583236961
shenyushi <3497386841@qq.com>
2021-08-09 04:32:37 +0800

    finish q1206

--- leetcode/q546/src/main.rs
+++ leetcode/q546/src/main.rs
@@ -7,13 +7,13 @@ impl Solution {
         let len = boxes.len();
         let mut dp = vec![vec![]; len];
         for (i, v) in dp.iter_mut().enumerate() {
-            v.resize(len - i + 1, vec![]);
+            v.resize(len - i, vec![]);
             for (j, w) in v.iter_mut().enumerate() {
                 w.resize(len - i - j + 1, 0);
             }
         }
         let mut sol = Sol { dp, boxes };
-        sol.solve(0, len, 0)
+        sol.solve(0, len - 1, 0)
     }
 }
 
@@ -23,17 +23,23 @@ struct Sol {
 }
 
 impl Sol {
-    fn solve(&mut self, i: usize, j: usize, k: usize) -> i32 {
-        if j == 0 {
-            return 0;
+    #[inline]
+    fn check_solve(&mut self, i: usize, j: usize, minus: usize, k: usize) -> i32 {
+        if j < minus {
+            0
+        } else {
+            self.solve(i, j - minus, k)
         }
+    }
+
+    fn solve(&mut self, i: usize, j: usize, k: usize) -> i32 {
         if self.dp[i][j][k] == 0 {
-            let mut opt = self.solve(i, j - 1, 0) + ((k + 1) * (k + 1)) as i32;
-            for l in (1..j).rev() {
-                if self.boxes[i + l - 1] == self.boxes[i + j - 1] {
+            let mut opt = self.check_solve(i, j, 1, 0) + ((k + 1) * (k + 1)) as i32;
+            for l in (0..j).rev() {
+                if self.boxes[i + l] == self.boxes[i + j] {
                     opt = max(
                         opt,
-                        self.solve(i, l, k + 1) + self.solve(i + l, j - l - 1, 0),
+                        self.solve(i, l, k + 1) + self.check_solve(i + l + 1, j, l + 2, 0),
                     );
                 }
             }
@@ -50,5 +56,7 @@ fn main() {
         1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 1, 1,
         1, 1, 1, 1, 1, 2, 1, 2, 2, 1,
     ];
+    // let boxes = vec![1, 3, 2, 2, 2, 3, 4, 3, 1];
+    // let boxes = vec![1, 1];
     println!("{}", Solution::remove_boxes(boxes))
 }
